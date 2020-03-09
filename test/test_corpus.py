dad8eeac50e77756b3e868e362e1b0b99cb3069b
Julien Baley <julien.baley@gmail.com>
2020-03-09 09:06:35 +0000

    Add "filter_by_authors" function to filter corpus
    This allows filtering by a single author or a group thereof.

--- test/test_corpus.py
+++ test/test_corpus.py
@@ -1,6 +1,6 @@
 import unittest
 
-from ds import corpus
+from ds import authors, corpus
 
 
 class TestCorpus(unittest.TestCase):
@@ -10,3 +10,35 @@ class TestCorpus(unittest.TestCase):
         self.assertTrue(all('title' in poem for poem in qts))
         self.assertTrue(all('paragraphs' in poem for poem in qts))
         self.assertGreater(len(qts), 50000)
+
+    def test_filter_by_single_author(self):
+        qts = corpus.get_corpus('qts')
+        auths = authors.load_authors('qts')
+        auths = [auths['歐陽詹'][0], auths['韓愈'][0]]
+
+        oyz = corpus.filter_by_authors('歐陽詹', qts)
+
+        self.assertTrue(poem['author'] == '歐陽詹'
+                        for poem in oyz)
+
+    def test_filter_by_multiple_string_authors(self):
+        qts = corpus.get_corpus('qts')
+        auths = authors.load_authors('qts')
+        auths = [auths['歐陽詹'][0], auths['韓愈'][0]]
+
+        oyz_hy = corpus.filter_by_authors({'歐陽詹', '韓愈'}, qts)
+
+        self.assertNotEqual(0, len(oyz_hy))
+        self.assertTrue(poem['author'] == '歐陽詹' or poem['author'] == '韓愈'
+                        for poem in oyz_hy)
+
+    def test_filter_by_multiple_dict_authors(self):
+        qts = corpus.get_corpus('qts')
+        auths = authors.load_authors('qts')
+        auths = [auths['歐陽詹'][0], auths['韓愈'][0]]
+
+        oyz_hy = corpus.filter_by_authors(auths, qts)
+
+        self.assertNotEqual(0, len(oyz_hy))
+        self.assertTrue(poem['author'] == '歐陽詹' or poem['author'] == '韓愈'
+                        for poem in oyz_hy)
