74047756252d4c299dd4bfec621850b191780a09
Julien Baley <julien.baley@gmail.com>
2020-05-03 18:16:06 +0100

    Fix passing Node to igraph and reading back from it (#36)
    We have to convert Node to a string, and then parse it back.

--- test/test_community.py
+++ test/test_community.py
@@ -4,33 +4,6 @@ from collections import Counter
 from ds import community
 
 
-class TestGetCommunityRhymeCategories(unittest.TestCase):
-    def test_get_community_rhyme_categories(self):
-        prons = {
-            '深': [{'GY Rhyme': '侵'}, {'GY Rhyme': '沁'}],
-            '心': [{'GY Rhyme': '侵'}],
-            '金': [{'GY Rhyme': '侵'}],
-            '簪': [{'GY Rhyme': '侵'}, {'GY Rhyme': '覃'}],
-
-            '沒': [{'GY Rhyme': '沒'}],
-            '發': [{'GY Rhyme': '月'}],
-            '室': [{'GY Rhyme': '質'}],
-            '骨': [{'GY Rhyme': '沒'}],
-            '佛': [{'GY Rhyme': '物'}],
-            '別': [{'GY Rhyme': '薛'}, {'GY Rhyme': '薛'}],
-            '曰': [{'GY Rhyme': '月'}],
-        }
-        communities = [{'深', '心', '金', '簪'},
-                       {'沒', '發', '室', '骨', '佛', '別', '曰'}]
-
-        self.assertEquals(
-            Counter({'侵': 4, '沁': 1, '覃': 1}),
-            community.get_community_rhyme_categories(communities[0], prons))
-        self.assertEquals(
-            Counter({'月': 2, '沒': 2, '質': 1, '薛': 1, '物': 1}),
-            community.get_community_rhyme_categories(communities[1], prons))
-
-
 class TestAlignCommunities(unittest.TestCase):
     def test_align_communities(self):
         corpus_a_comms = [Counter({'侵': 4, '沁': 1, '覃': 1}),
